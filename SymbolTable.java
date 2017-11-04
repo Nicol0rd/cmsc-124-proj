052a8e3248c6e801149f66cd76ecd19c7862861c
quinn704 <baticosandricquinn@yahoo.com.ph>
2017-11-05 03:13:10 +0800

    added new updates

--- SymbolTable.java
+++ SymbolTable.java
@@ -12,7 +12,7 @@
 *	 Nicolo Jireh D. Unson																 *
 *                                                                                        *
 *  File Description:                                                                     *
-*  This file creates the interpreter panel for GUI.                                      *
+*  This file creates the symbol table panel for GUI.                                     *
 *                                                                                        *
 *  (c) ALL RIGHTS RESERVED 2017                                                          *
 *                                                                                        *
@@ -22,38 +22,25 @@
 	package gui;
 
 //	--------------------------------------------------	[JAVA IMPORTS]
-	import java.awt.BorderLayout;
 	import java.awt.Color;	
 	import java.awt.Dimension;
 	import javax.swing.JPanel;
 	import gui.*;
 
 //	--------------------------------------------------	[CLASS SPECIFICATION]
-public class Interpreter extends JPanel {
+public class SymbolTable extends JPanel {
 
 	/**************
 	*  Constants  *
 	**************/
-	private static final int MAXIMUM_PANEL_WIDTH = 600;
-	private static final int MAXIMUM_PANEL_HEIGHT = MainPanel.getPanelHeight();
+	private static final int MAXIMUM_PANEL_WIDTH = Interpreter.getPanelWidth()/2;
+	private static final int MAXIMUM_PANEL_HEIGHT = Interpreter.getPanelHeight();
 
-	/***************
-	*  Attributes  *
-	***************/
-	
-	// Main Panels
-		private JPanel titlebar;
-		private LexemeTable lexeme;
-		private SymbolTable symboltable;
-
-	// Panel - Dividers
-		private JPanel topPanel;
-		private JPanel bottomPanel;
 
 	/***************************
 	*    Panel Constructors    *
 	***************************/
-	public Interpreter() {
+	public SymbolTable() {
 		super();
 		this.setComponents();
 	}
@@ -63,33 +50,14 @@ public class Interpreter extends JPanel {
 	****************/
 	private void setComponents() {
 
-		this.setPreferredSize(new Dimension(Interpreter.MAXIMUM_PANEL_WIDTH, Interpreter.MAXIMUM_PANEL_HEIGHT));
-		this.setLayout(new BorderLayout());
-
-		// creating components
-		this.topPanel = new JPanel(); this.bottomPanel = new JPanel();	
-		this.bottomPanel.setLayout(new BorderLayout());
-		this.topPanel.setLayout(new BorderLayout());
-
-		this.titlebar = new JPanel();
-		this.lexeme = new LexemeTable();
-		this.symboltable = new SymbolTable();
-		
-		this.titlebar.setBackground(Color.BLACK);
-		this.titlebar.setPreferredSize(new Dimension(Interpreter.MAXIMUM_PANEL_WIDTH, Interpreter.MAXIMUM_PANEL_HEIGHT-250));
-
-		this.bottomPanel.add(lexeme, BorderLayout.WEST);
-		this.bottomPanel.add(symboltable, BorderLayout.EAST);
-		this.topPanel.add(titlebar, BorderLayout.CENTER);
-
-		this.add(topPanel, BorderLayout.NORTH);
-		this.add(bottomPanel, BorderLayout.SOUTH);
+		this.setPreferredSize(new Dimension(SymbolTable.MAXIMUM_PANEL_WIDTH, SymbolTable.MAXIMUM_PANEL_HEIGHT));
+		this.setBackground(Color.YELLOW);
 	}
 
 	/****************
 	*    Getters    *
 	****************/
-	public static int getPanelWidth(){return Interpreter.MAXIMUM_PANEL_WIDTH;}
-	public static int getPanelHeight(){return Interpreter.MAXIMUM_PANEL_HEIGHT;}
+	public static int getPanelWidth(){return SymbolTable.MAXIMUM_PANEL_WIDTH;}
+	public static int getPanelHeight(){return SymbolTable.MAXIMUM_PANEL_HEIGHT;}
 
 }
